a951198937b85dcaa353f2ab5ec410e582382e2a
Salvador Nicolas <salvadorn.1111@gmail.com>
2022-03-14 13:36:56 -0300

    Subo desafio navbar´

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () =>{
+    return(
+        <nav className='navbar'>
+                <img src='https://agrogojarviveros.com/entradavivero/uploads/2017/07/herbal-spa-treatment-leaves.png' alt='logo' className='logo'/>
+            <a className='linksnav' href="#">Inicio</a>
+            <a className='linksnav' href="#">Nosotros</a>
+            <a className='linksnav' href="#">Productos</a>
+            <a className='linksnav' href="#">Contactanos</a>
+        </nav>
+    )
+}
+
+export default Navbar
